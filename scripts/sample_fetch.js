a57b97771fba1dd6b39f59bae973ab95d9a27a88
Kinjal Ray <kinjalray10@gmail.com>
2019-12-12 19:57:23 +0530

    ammended solution

--- scripts/sample_fetch.js
+++ scripts/sample_fetch.js
@@ -8,25 +8,22 @@
   let list = "";
   posts.map((post) => {
     list += `<input type='checkbox' name="${post.id}" id="${post.id}"><span id="title_${post.id}">${post.title}</span></input><br>`
-  })
+  });
   document.getElementById("posts").innerHTML = list;
 
-  /*editing posts by PUT requests*/
+  /*adding checkbox event*/
   posts.map((post) => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      title: 'avengers',
-      body: 'assemble',
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  .then(response => {
-      /*check and strikeout the post*/
-      document.getElementById(`${post.id}`).checked = true;
-      document.getElementById(`title_${post.id}`).setAttribute("style" , "text-decoration: line-through;");
-    })
-  })
+    document.getElementById(`${post.id}`).addEventListener("click" , () => {
+      if(document.getElementById(`${post.id}`).checked)
+      {
+        /*strikeout the post*/
+        document.getElementById(`title_${post.id}`).setAttribute("style" , "text-decoration: line-through;");
+      }
+      else
+      {
+        /*remove strikeout from the post*/
+        document.getElementById(`title_${post.id}`).setAttribute("style" , "text-decoration: none;");
+      }
+    });
+  });
 })();
